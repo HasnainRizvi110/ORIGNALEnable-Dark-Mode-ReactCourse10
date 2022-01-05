aeea5b8bebdbfeb010daf80e7daf05db18731c58
HasnainRizvi <96916765+HasnainRizvi110@users.noreply.github.com>
2022-01-05 13:29:59 +0500

    src/App.js file

---
+++ Components/App.js
@@ -0,0 +1,31 @@
+import './App.css';
+import About from './components/About';
+import Navbar from './components/Navbar';
+import TextForm from './components/TextForm';
+
+function App() {
+  return (
+  <>
+  
+
+
+<Navbar title="NavHome" />
+
+   <div className="container my-3">
+<TextForm  heading="Enter the text to analyze"/>
+
+----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+
+<About/>
+</div>
+
+
+
+
+  </>
+  );
+}
+
+export default App;
